09cb3c818b98c1c9e41bf3f2b3062b1dbdf211d8
Gregoire <ggguille80@gmail.com>
2022-05-14 18:46:59 +0200

    Add icao number for supported flights

--- configuration/__init__.py
+++ configuration/__init__.py
@@ -4,11 +4,11 @@ AIRLABS_URL = "https://airlabs.co/api/v9"
 ORION_URL = "http://impossibly.fr:1026/v2"
 # We choose only five flights to limit api calls.
 SUPPORTED_FLIGHTS = [
-    {"dep_iata": "CDG", "arr_iata": "JFK", "description": "from Paris to New York"},
-    {"dep_iata": "CDG", "arr_iata": "MLA", "description": "from Paris to Malte"},
-    {"dep_iata": "CDG", "arr_iata": "MRS", "description": "from Paris to Marseille"},
-    {"dep_iata": "IBZ", "arr_iata": "BCN", "description": "from Ibiza to Barcelona"},
-    {"dep_iata": "OTP", "arr_iata": "PMO", "description": "from Bucarest to Palermo"},
+    {"dep_iata": "CDG", "arr_iata": "JFK", "description": "from Paris to New York", "dep_icao": "LFPG", "arr_icao": "KJFK"},
+    {"dep_iata": "CDG", "arr_iata": "MLA", "description": "from Paris to Malte", "dep_icao": "LFPG", "arr_icao": "LMML"},
+    {"dep_iata": "CDG", "arr_iata": "MRS", "description": "from Paris to Marseille", "dep_icao": "LFPG", "arr_icao": "LFML"},
+    {"dep_iata": "IBZ", "arr_iata": "BCN", "description": "from Ibiza to Barcelona", "dep_icao": "LEIB", "arr_icao": "LEBL"},
+    {"dep_iata": "OTP", "arr_iata": "PMO", "description": "from Bucarest to Palermo", "dep_icao": "LROP", "arr_icao": "LICJ"},
 ]
 # We choose only a few airports to limit the number of requests to airlabs API
-SUPPORTED_AIRPORTS = ["CDG", "JFK", "MLA", "MRS", "BCN", "OTP", "PMO"]
+SUPPORTED_AIRPORTS = ["CDG", "JFK", "MLA", "MRS", "BCN", "OTP", "PMO", "IBZ"]
