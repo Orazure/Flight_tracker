991a3c5a7b6b74d8c52352e14b2d11352250935a
Gregoire <ggguille80@gmail.com>
2022-05-14 18:47:34 +0200

    FIX the filter we have to use icao-number because of the relationship

--- DAL/flights.py
+++ DAL/flights.py
@@ -26,23 +26,23 @@ class FlightDAL:
         params = {"type": "Flight"}
         return requests.get(f"{self.orion_url}/{flight_id}", params=params).json()
 
-    def get_flights_from_iata(self, dep_iata: str, arr_iata: str) -> List[dict]:
-        """Get flight from departure and arrival IATA codes.
+    def get_flights_from_icao(self, dep_icao: str, arr_icao: str) -> List[dict]:
+        """Get flight from departure and arrival ICAO codes.
 
         Args:
-            dep_iata (str): IATA code of the departure airport.
-            arr_iata (str): IATA code of the arrival airport.
+            dep_icao (str): ICAO code of the departure airport.
+            arr_icao (str): ICAO code of the arrival airport.
 
         Returns:
             List[dict]: The flights.
 
         Example:
             >>> dal = FlightDAL()
-            >>> dal.get_flights_from_iata('LHR', 'CDG')
-            [{'id': 'Flight_1', 'dep_iata': 'LHR', 'arr_iata': 'CDG'...]
+            >>> dal.get_flights_from_icao('LFPG', 'KJFK')
+            [{'id': 'Flight_1', 'departsFromAirport.value': 'LFPG', ...]
         """
         params = {
             "type": "Flight",
-            "q": f"dep_iata=={dep_iata} AND arr_iata=={arr_iata}",
+            "q": f"departsFromAirport==airport-{dep_icao};arrivesToAirport==airport-{arr_icao}",
         }
         return requests.get(f"{self.orion_url}", params=params).json()
