2cdd0dc22030e7697bf4941417d30ab3914c22cb
KnockAce <ggguille80@gmail.com>
2022-05-16 09:51:47 +0200

    Update airlabs_api.py

--- airlabs_api.py
+++ airlabs_api.py
@@ -1,11 +1,13 @@
 from airlabs.airports_importer import update_airports_info
 from airlabs.flight_importer import update_flights_information
 from airlabs.live_flight_importer import update_live_flights
+from airlabs.airlines_importer import add_airline_information
 
 
 def main() -> None:
     """Main function."""
     # update_airports_info()
+    # add_airline_information() # One shot function the data will not be often updated.
     update_flights_information()
     update_live_flights()
 
