3ff6f09ed2cd4e186239eb636796957803f389b7
Orazure <thealactraz@gmail.com>
2022-05-16 10:21:04 +0200

    ok

--- apps/configuration/__init__.py
+++ apps/configuration/__init__.py
@@ -4,12 +4,51 @@ AIRLABS_URL = "https://airlabs.co/api/v9"
 ORION_URL = "http://impossibly.fr:1026/v2"
 # We choose only five flights to limit api calls.
 SUPPORTED_FLIGHTS = [
-    { "0" :{"dep_iata": "CDG", "arr_iata": "JFK", "description": "from Paris to New York"}},
-    { "1" :{"dep_iata": "CDG", "arr_iata": "MLA", "description": "from Paris to Malte"}},
-    {"2" :{"dep_iata": "CDG", "arr_iata": "MRS", "description": "from Paris to Marseille"}},
-    {"3" :{"dep_iata": "IBZ", "arr_iata": "BCN", "description": "from Ibiza to Barcelona"}},
-    {"4" :{"dep_iata": "OTP", "arr_iata": "PMO", "description": "from Bucarest to Palermo"}},
-    {"5" :{"dep_iata": "LFPG", "arr_iata": "KJFK", "description": "from KJFK to AFR"}},
+    {
+        "0": {
+            "dep_iata": "CDG",
+            "arr_iata": "JFK",
+            "description": "from Paris to New York",
+            "dep_icao": "LFPG",
+            "arr_icao": "KJFK",
+        }
+    },
+    {
+        "1": {
+            "dep_iata": "CDG",
+            "arr_iata": "MLA",
+            "description": "from Paris to Malte",
+            "dep_icao": "LFPG",
+            "arr_icao": "LMML",
+        }
+    },
+    {
+        "2": {
+            "dep_iata": "CDG",
+            "arr_iata": "MRS",
+            "description": "from Paris to Marseille",
+            "dep_icao": "LFPG",
+            "arr_icao": "LFML",
+        }
+    },
+    {
+        "3": {
+            "dep_iata": "IBZ",
+            "arr_iata": "BCN",
+            "description": "from Ibiza to Barcelona",
+            "dep_icao": "LEIB",
+            "arr_icao": "LEBL",
+        }
+    },
+    {
+        "4": {
+            "dep_iata": "OTP",
+            "arr_iata": "PMO",
+            "description": "from Bucarest to Palermo",
+            "dep_icao": "LROP",
+            "arr_icao": "LICJ",
+        }
+    },
 ]
 # We choose only a few airports to limit the number of requests to airlabs API
 SUPPORTED_AIRPORTS = ["CDG", "JFK", "MLA", "MRS", "BCN", "OTP", "PMO"]
