557ed2d07d20e23ebc2850021cbcab1051f69f73
KnockAce <ggguille80@gmail.com>
2022-05-17 14:04:16 +0200

    update ORION_URL to directly query on the docker

--- apps/configuration/__init__.py
+++ apps/configuration/__init__.py
@@ -1,7 +1,7 @@
 API_KEY = "9a00f99a-81f1-4a9b-a2f0-53c1d6f30d14"
 AIRLABS_URL = "https://airlabs.co/api/v9"
-# ORION_URL = "http://localhost:1026/v2"
-ORION_URL = "http://impossibly.fr:1026/v2"
+ORION_URL = "http://orion:1026/v2"
+# ORION_URL = "http://impossibly.fr:1026/v2"
 # We choose only five flights to limit api calls.
 SUPPORTED_FLIGHTS = [
     {
