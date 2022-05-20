76c51d777fb2af99b8c8a2e970551791238fd06f
Orazure <thealactraz@gmail.com>
2022-05-20 10:35:16 +0200

    ok

--- apps/DAL/dataFromMysql.py
+++ apps/DAL/dataFromMysql.py
@@ -44,30 +44,3 @@ class Database:
     def query(self, sql, params=None):
         self.cursor.execute(sql, params or ())
         return self.fetchall()
-# try:
-#     connection = mysql.connector.connect(host='localhost',
-#                                          database='electronics',
-#                                          user='pynative',
-#                                          password='pynative@#29')
-
-#     sql_select_Query = "select * from Laptop"
-#     cursor = connection.cursor()
-#     cursor.execute(sql_select_Query)
-#     # get all records
-#     records = cursor.fetchall()
-#     print("Total number of rows in table: ", cursor.rowcount)
-
-#     print("\nPrinting each row")
-#     for row in records:
-#         print("Id = ", row[0], )
-#         print("Name = ", row[1])
-#         print("Price  = ", row[2])
-#         print("Purchase date  = ", row[3], "\n")
-
-# except mysql.connector.Error as e:
-#     print("Error reading data from MySQL table", e)
-# finally:
-#     if connection.is_connected():
-#         connection.close()
-#         cursor.close()
-#         print("MySQL connection is closed")
